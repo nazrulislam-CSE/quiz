cc39cd3e3d816d4dfbb687b274487402cdd74224
nazrulislam-CSE <nsuzon02@gmail.com>
2025-08-07 23:20:07 +0600

    some bugs solved

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -6,74 +6,10 @@
                 <a class="close-toggle" href="javascript:void(0);"><i class="header-icons" data-eva="close-outline"></i></a>
             </div>
             <div class="responsive-logo">
-                <h5 class="text-uppercase font-weight-bolder text-light">Speak Up BD </h5>
+                <h5 class="text-uppercase font-weight-bolder text-light">Quiz</h5>
             </div>
             <ul class="header-megamenu-dropdown  nav">
                 <a href="{{ route('frontend.home')}}" target="_blank" class="btn btn-success text-center"><i class="fa fa-share"></i> View Website</a>
-                {{-- <li class="nav-item">
-                    <div class="btn-group dropdown">
-                        <button aria-expanded="false" aria-haspopup="true" class="btn btn-link dropdown-toggle" data-bs-toggle="dropdown" id="dropdownMenuButton2" type="button"><span><i class="fe fe-settings"></i> Settings </span></button>
-                        <div class="dropdown-menu">
-                            <div class="dropdown-menu-header header-img p-3">
-                                <div class="drop-menu-inner">
-                                    <div class="header-content text-start d-flex">
-                                        <div class="text-white">
-                                            <h5 class="menu-header-title">Setting</h5>
-                                            <h6 class="menu-header-subtitle mb-0">Overview of theme</h6>
-                                        </div>
-                                        <div class="my-auto ms-auto">
-                                            <span class="badge bg-pill bg-warning float-end">View all</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="setting-scroll">
-                                <div>
-                                    <div class="setting-menu ">
-                                        <a class="dropdown-item" href="profile.html"><i class="mdi mdi-account-outline tx-16 me-2 mt-1"></i>Profile</a>
-                                        <a class="dropdown-item" href="contacts.html"><i class="mdi mdi-account-box-outline tx-16 me-2"></i>Contacts</a>
-                                        <a class="dropdown-item" href="settings.html"><i class="mdi mdi-account-location tx-16 me-2"></i>Accounts</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="about.html"><i class="typcn typcn-briefcase tx-16 me-2"></i>About us</a>
-                                        <a class="dropdown-item" href="javascript:void(0);"><i class="mdi mdi-application tx-16 me-2"></i>Getting start</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <ul class="setting-menu-footer flex-column ps-0">
-                                <li class="divider mb-0 pb-0 "></li>
-                                <li class="setting-menu-btn">
-                                    <button class=" btn-shadow btn btn-success btn-sm">Cancel</button>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <div class="dropdown-menu-rounded btn-group dropdown">
-                        <button aria-expanded="false" aria-haspopup="true" class="btn btn-link dropdown-toggle" data-bs-toggle="dropdown" id="dropdownMenuButton3" type="button"><span><i class="nav-link-icon fe fe-briefcase"></i> Projects </span></button>
-                        <div class="dropdown-menu-lg dropdown-menu" x-placement="bottom-left">
-                            <div class="dropdown-menu-header">
-                                <div class="dropdown-menu-header-inner header-img p-3">
-                                    <div class="header-content text-start d-flex">
-                                        <div class="text-white">
-                                            <h5 class="menu-header-title">Projects</h5>
-                                            <h6 class="menu-header-subtitle mb-0">Overview of Projects</h6>
-                                        </div>
-                                        <div class="my-auto ms-auto">
-                                            <span class="badge bg-pill bg-warning float-end">View all</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a class="dropdown-item  mt-2" href="javascript:void(0);"><i class="dropdown-icon"></i>Mobile Application</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="dropdown-icon"></i>PSD Projects</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="dropdown-icon"></i>PHP Project</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="dropdown-icon"></i>Wordpress Projects</a>
-                            <a class="dropdown-item mb-2" href="javascript:void(0);"><i class="dropdown-icon "></i>HTML & CSS3 Projects</a>
-                        </div>
-                    </div>
-                </li> --}}
             </ul>
         </div>
         <button class="navbar-toggler nav-link icon navresponsive-toggler vertical-icon ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
