cadcffcf0980bcff944c8f67ede0122694bd7dfe
nazrulislam-CSE <nsuzon02@gmail.com>
2025-08-25 14:58:32 +0600

    some error solved

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Carbon;
 use App\Models\Slider;
 use App\Models\Counter;
 use App\Models\About;
+use App\Models\Teacher;
+use App\Models\Student;
 
 class FrontendController extends Controller
 {
@@ -18,9 +20,11 @@ class FrontendController extends Controller
         $abouts = About::where('status',1)->latest()->get();
         $counters = Counter::where('status',1)->latest()->get();
         $abouts = About::where('status',1)->latest()->get();
+        $teachers = Teacher::where('status',1)->latest()->get();
+        $students = Student::where('status',1)->latest()->get();
     
         $pageTitle = 'Home';
-        return view('frontend.index',compact('pageTitle','sliders','abouts','counters'));
+        return view('frontend.index',compact('pageTitle','sliders','abouts','counters','teachers','students'));
     }
 
 }
