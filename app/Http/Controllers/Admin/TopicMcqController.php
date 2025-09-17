be3b9ccbb9a968c3b969fc9b1ed84545b69ab807
nazrulislam-CSE <nsuzon02@gmail.com>
2025-09-17 18:45:17 +0600

    some update

--- app/Http/Controllers/Admin/TopicMcqController.php
+++ app/Http/Controllers/Admin/TopicMcqController.php
@@ -190,7 +190,7 @@ class TopicMcqController extends Controller
     // AJAX Methods for Dynamic Dropdowns
     public function getDepartments($admission_id)
     {
-        $departments = Department::whereHas('admissions', function ($query) use ($admission_id) {
+        $departments = Department::whereHas('admission', function ($query) use ($admission_id) {
             $query->where('admission_id', $admission_id);
         })->where('status', '1')->get();
 
@@ -198,7 +198,7 @@ class TopicMcqController extends Controller
     }
     public function getSubjects($department_id)
     {
-        $subjects = Subject::whereHas('departments', function ($query) use ($department_id) {
+        $subjects = Subject::whereHas('department', function ($query) use ($department_id) {
             $query->where('department_id', $department_id);
         })->where('status', '1')->get();
 
