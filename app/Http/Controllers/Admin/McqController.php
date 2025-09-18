acde70ac4fece59bcbb449d51392ca70645c5bca
nazrulislam-CSE <nsuzon02@gmail.com>
2025-09-18 22:29:21 +0600

    v2

--- app/Http/Controllers/Admin/McqController.php
+++ app/Http/Controllers/Admin/McqController.php
@@ -40,7 +40,7 @@ class McqController extends Controller
             'exam_duration' => 'required',
             'exam_mark' => 'required',
             'questions' => 'required|array|min:1',
-            'questions.*.answer' => 'required|string',
+            'questions.*.text' => 'required|string',
             'questions.*.answers' => 'required|array|min:4', // Ensure exactly 4 answers
             'questions.*.answers.*.answer' => 'required|string', // Changed to match your input structure
             'questions.*.correct_answer' => 'required|integer|between:0,3', // Must be 0-3 (for 4 options)
